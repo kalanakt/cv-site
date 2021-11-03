98657140e5610de36427f91cc69a942bd014ce2d
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-11-03 18:08:54 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -21,14 +21,13 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+
   $contact->smtp = array(
     'host' => 'kalanakithmina@hotmail.com',
     'username' => 'kalanakithmina@hotmail',
     'password' => 'pass',
     'port' => '587'
   );
-  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
