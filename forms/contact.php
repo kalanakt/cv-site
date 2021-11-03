6376b212334fda742aa6a963da6ee32c17b11d28
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-11-03 18:35:35 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -3,12 +3,6 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'kalanakithmina@hotmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
   
