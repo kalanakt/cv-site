53b9709cd41c89e233c051c9816dbf22faa1a9bc
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-11-03 15:45:08 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,9 +1,6 @@
 <?php
   /**
   * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
   */
 
   // Replace contact@example.com with your real receiving email address
