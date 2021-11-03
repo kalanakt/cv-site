1ce058a81bd80647c1fa9ce40bb343f5fa762b94
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-11-03 18:13:24 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -3,7 +3,7 @@
   * Requires the "PHP Email Form" library
   */
 
-  // Replace contact@example.com with your real receiving email address
+ 
   $receiving_email_address = 'kalanakithmina@hotmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
@@ -19,8 +19,6 @@
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
 
   $contact->smtp = array(
     'host' => 'kalanakithmina@hotmail.com',
