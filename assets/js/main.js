0f9c12b8f906d85e9e6ec04e12c129d9c12ac7ea
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-11-03 15:40:33 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,6 @@
 /**
-* Template Name: MyResume - v4.6.0
-* Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
+* MIT LICENE
+* copyright #Hash Minner
 */
 (function() {
   "use strict";
